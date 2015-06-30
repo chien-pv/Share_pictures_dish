7561a232a8d2993f110861b2120d1423dce6369f
VanChien <phamvanbk11@gmail.com>
2015-06-30 10:53:48 +0700

    chuc nang comment

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -10,6 +10,7 @@ class DishesController < ApplicationController
   # GET /dishes/1
   # GET /dishes/1.json
   def show
+  @commend = Commend.new
   end
 
   # GET /dishes/new
---
+++ test/controllers/commends_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommendsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
